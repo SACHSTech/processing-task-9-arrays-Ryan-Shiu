dd5728a77a60fe03f68aebdabe8339adf5db3bed
Ryan-Shiu <ryan.shiu23@ycdsbk12.ca>
2022-05-12 22:25:19 -0400

    Completed task 9

--- Sketch.java
+++ Sketch.java
@@ -3,12 +3,22 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 	
   float[] circleY = new float[25];
+  float[] circleX = new float[25];
+  boolean[] ballhidestatus = new boolean[25];
   float rectX = 50;
   float rectY = 50;
   float playerX = 300;
   float playerY = 300;
-  boolean hit1;
-  float circleRadius;
+  int lives = 3;
+  boolean playerDead = false;
+  boolean upPressed = false;
+  boolean downPressed = false;
+  boolean rightPressed = false;
+  boolean leftPressed = false;
+  boolean wPressed = false;
+  boolean aPressed = false;
+  boolean sPressed = false;
+  boolean dPressed = false;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -24,9 +34,13 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-      size(600, 600);
-      for (int i = 0; i < circleY.length; i++) {
-        circleY[i] = random(height);
+    size(600, 600);
+
+    // Starting values of the snowballs at different placements on screen. 
+    for (int i = 0; i < circleY.length; i++) {
+      circleX[i] = random(width);
+      circleY[i] = random(height);
+      ballhidestatus[i] = false;
       }
 
     }
@@ -35,23 +49,28 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
+	  // Black background
     background(50);
 
+    // Drawing the main player which is a blue circle.
     fill(0, 37, 250);
     ellipse(playerX, playerY, 25, 25);
 
+    // A for loop that draws each snowball
     for (int i = 0; i < circleY.length; i++) {
-      float circleX = width * i / circleY.length;
       fill(255, 255, 255);
-      ellipse(circleX, circleY[i], 50, 50);
+      ellipse(circleX[i], circleY[i], 50, 50);
   
+      // The snowballs fall 2 units. 
       circleY[i]+=2;
   
+      // When the snowball goes off the screen it respawns back at the top and falls again.
       if (circleY[i] > height) {
         circleY[i] = 0;
       }
-
+      
+      // When the key down is pressed the snowballs move faster.
+      // When the key up is pressed the snowballs move slower.
       if(keyPressed){
         if(keyCode == UP){
           circleY[i] = circleY[i] - 1;
@@ -59,7 +78,9 @@ public class Sketch extends PApplet {
         else if(keyCode == DOWN){
           circleY[i] = circleY[i] + 2;
         }
-        else if(key == 'w'){
+        
+        // The following if and if else statements determine player movement with wasd.
+        if(key == 'w'){
           playerY -= 0.1;
         }
         else if(key == 'a'){
@@ -71,27 +92,83 @@ public class Sketch extends PApplet {
         else if(key == 'd'){
           playerX += 0.1;
         }
+      }
 
       }
-      //fill(255, 7, 3);
-
-      //rect(525, 15, rectX, rectY);
-     // rect(450, 15, rectX, rectY);
-    
-      if(hit1 = true){
+        // Drawing the 3 red squares that represents lives remaining.
         fill(255, 7, 3);
         rect(375, 15, rectX, rectY);
+        rect(525, 15, rectX, rectY);
+        rect(450, 15, rectX, rectY);
+
+
+      // Collision detection where if the player hits a snowball they lose a life.
+      for(int count = 0; count < circleY.length; count++){
+        if(dist(playerX, playerY, (circleX[count]), circleY[count]) <= 37.5){
+           circleY[count] = 0;
+           lives--;
+           ballhidestatus[count] = true;
+       }
       }
-      
-      if(dist(playerX, playerY, circleX, circleY[i]) < 25){
-        System.out.println("true");
+
+      // When the player looses a life they also lose a red square.
+      if(lives == 2){
         fill(0, 0, 0);
         rect(375, 15, rectX, rectY);
-        hit1 = false;
+      }
+
+      else if (lives == 1){
+        fill(0, 0, 0);
+        rect(375, 15, rectX, rectY);
+        rect(450, 15, rectX, rectY);
+      }
+
+      else if(lives == 0){
+        playerDead = true;
+      }
+      
+      // When the player runs out of lives the screen turns white.
+      if(playerDead == true){
+        background(255, 255, 255);
+      }
+
+    // If the user clicks on a snowball it disappears and respawns back to the top.
+    for(int count = 0; count < circleY.length; count++){
+      if(mousePressed){
+        if(dist(mouseX, mouseY,(circleX[count]), circleY[count]) <= 37.5){
+          circleY[count] = 0;
+          ballhidestatus[count] = true;
       }
     }
-
   }
   
   // define other methods down here.
+  }
+  // Multiple key handling
+  public void keyPressed(){
+    if(keyCode == UP){
+      upPressed = true;
+    }
+    if(keyCode == DOWN){
+      downPressed = true;
+    }
+    if(keyCode == RIGHT){
+      rightPressed = true;
+    }
+    if(keyCode == LEFT){
+      leftPressed = true;
+    }
+    if(keyCode == 'w'){
+      wPressed = true;
+    }
+    if(keyCode == 'a'){
+      aPressed = true;
+    }
+    if(keyCode == 's'){
+      sPressed = true;
+    }
+    if(keyCode == 's'){
+      sPressed = true;
+    }
+  }
 }
